f6de82eb05b1a1932416e89a7d34729e9c13f004
Wong Cho Hing <venus.wong@oocl.com>
2018-11-19 23:47:43 +0800

    Update main.js
    Finished

--- main/main.js
+++ main/main.js
@@ -5,28 +5,29 @@ module.exports = function main(inputs) {
     var counted_item = [];
     var count = 0;
     var total = 0;
-    var item;
+
     var expectTest = '***<store earning no money>Receipt ***\n';
 
     inputs.forEach(function (itemLoop1){
-        if (counted_item.includes(itemLoop1.Name))
-            break;
-        else
-            item = itemLoop1.Name;
-            counted_item.push(itemLoop1);
+        if (counted_item.includes(itemLoop1.Name)){
+        }
+        else{
+            counted_item.push(itemLoop1.Name);
             inputs.forEach(function (itemLoop2) {
-                if (itemLoop2 == itemLoop1){
-                    count++;
+                if (itemLoop2.Name == itemLoop1.Name){
+                    count += 1;
                 }
             });
             if (itemLoop1.Unit != 'a')
-                expectTest += 'Name: ' + itemLoop1.Name + ', Quantity: ' + itemLoop1.count + ' ' + itemLoop1.Unit + 's, Unit price: ' + itemLoop1.Price + ' (yuan), Subtotal: ' + itemLoop1.Price*count + ' (yuan)\n';
+                expectTest += 'Name: ' + itemLoop1.Name + ', Quantity: ' + count + ' ' + itemLoop1.Unit + 's, Unit price: ' + itemLoop1.Price.toFixed(2) + ' (yuan), Subtotal: ' + ((itemLoop1.Price)*count).toFixed(2) + ' (yuan)\n';
             else
-                expectTest += 'Name: ' + itemLoop1.Name + ', Quantity: ' + itemLoop1.count + ', Unit price: ' + itemLoop1.Price + ' (yuan), Subtotal: ' + itemLoop1.Price*count + ' (yuan)\n';
-            total += itemLoop1.Price*count;
+                expectTest += 'Name: ' + itemLoop1.Name + ', Quantity: ' + count + ', Unit price: ' + itemLoop1.Price.toFixed(2) + ' (yuan), Subtotal: ' + ((itemLoop1.Price)*count).toFixed(2) + ' (yuan)\n';
+            total += (itemLoop1.Price)*count;
+            count = 0;
+            }
     });
     expectTest += '----------------------\n' +
-    'Total: ' + total + ' (yuan)\n' +
+    'Total: ' + total.toFixed(2) + ' (yuan)\n' +
     '**********************\n'
     console.log(expectTest);
     return expectTest;
